05f8c4fc0d871977930181ab7fa7cab0c4ebf676
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-11-23 16:33:44 +0600

    Add project files.

---
+++ UnsaveCode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UnsaveCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  
+             C# allows using pointer variables in a function of code block when it is marked by the unsafe modifier.
+             The unsafe code or the unmanaged code is a code block that uses a pointer variable.
+             */
+            unsafe
+            {
+                int var = 20;
+                int* p = &var;
+
+                Console.WriteLine("Data is: {0} ", var);
+                Console.WriteLine("Data is: {0} ", p->ToString());
+                Console.WriteLine("Address is: {0} ", (int)p);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
